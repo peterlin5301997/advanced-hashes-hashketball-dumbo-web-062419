9a2f1db34c46cccdf8d6fe8da08232be8bde7e01
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-20 18:25:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -196,6 +196,9 @@ def winning_team
   sum_points = 0
   game_hash.each do |locaiton, team_data|
     team_data.each do |attribute, data|
+      if attribute == :team_name
+        team_name = data
+      end
       if attribute == :players
         data.each do |player, stats|
           stats.each do |stat, info|
