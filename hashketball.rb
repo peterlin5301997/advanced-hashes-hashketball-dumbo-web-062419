17a74b2d9cb0a3a34728d63a6a764aeb5afbde8f
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-20 18:20:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -172,6 +172,7 @@ end
 def most_points_scored
   most_points_scored = -1
   most_points_player = ""
+  player_name = ""
   game_hash.each do |location, team_data|
     team_data.each do |attribute, data|
       if attribute == :players
